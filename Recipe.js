40eb99d3af0ed3b1a1db72f451688e8162ca4207
Ephrem Girmay <ephremgirmay9@gamil.com>
2022-05-09 15:27:38 +0200

    structure

---
+++ Recipe.js
@@ -0,0 +1,62 @@
+/** @format */
+import React from "react";
+import style from "./recipe.module.css";
+
+import { GiRiceCooker } from "react-icons/gi";
+
+const Recipe = ({
+	title,
+	calories,
+	image,
+	dishType,
+	dietLabels,
+	source,
+	cusinType,
+	viewBtn,
+}) => {
+	return (
+		<div className={style.Recipe}>
+			<img className={style.Image} src={image} alt='' />
+			<div className={style.bgColor}></div>
+			<div className={style.Title}>
+				<h1>{title}</h1>
+			</div>
+			<div className={style.Text}>
+				<div className={style.Details}>
+					<p>
+						{" "}
+						<span className={style.detalTitle}>Cuisine Type</span> {cusinType}
+					</p>
+					<p>
+						{" "}
+						<span className={style.detalTitle}>Calories</span> {calories}
+					</p>
+					<p>
+						<span className={style.detalTitle}>Diet Labels</span>
+						{dietLabels}
+					</p>
+					<p>
+						<span className={style.detalTitle}>Dish Type</span>
+						{dishType}
+					</p>
+					<p>
+						<span className={style.detalTitle}>Source</span>
+						{source}
+					</p>
+					<a
+						className={style.viewbtn}
+						href={viewBtn}
+						target='_blank'
+						rel='noreferrer'
+					>
+						View recipe!{" "}
+						<div className={style.cookBtn}>
+							<GiRiceCooker />
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Recipe;
